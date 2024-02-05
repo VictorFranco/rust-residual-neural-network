66ec6ac7ffbbb2ce0400f4bfbc4d4e16de526109
Victor Franco <vfrancohdez1@hotmail.com>
2024-02-04 21:19:17 -0600

    feat(resnet): add Layer structure

--- src/resnet.rs
+++ src/resnet.rs
@@ -3,18 +3,17 @@ use crate::matrix::Matrix as Matrix;
 use rand::SeedableRng;
 
 #[derive(Debug, Clone)]
+pub struct Layer {
+    weights: Matrix,
+    biases: Matrix
+}
+
+#[derive(Debug, Clone)]
 pub struct ResNet {
     pub inputs: Tensor,
     pub labels: Tensor,
     pub rand: rand::rngs::StdRng,
-    pub w1: Matrix,
-    pub b1: Matrix,
-    pub w2: Matrix,
-    pub b2: Matrix,
-    pub w3: Matrix,
-    pub b3: Matrix,
-    pub w4: Matrix,
-    pub b4: Matrix,
+    pub layers: Vec<Layer>
 }
 
 impl ResNet {
@@ -23,15 +22,13 @@ impl ResNet {
         let inputs = inputs.clone();
         let labels = labels.clone();
         let mut rand = rand::rngs::StdRng::seed_from_u64(seed);
-        let w1 = Matrix::random_matrix(4,4,&mut rand);
-        let b1 = Matrix::random_matrix(1,4,&mut rand);
-        let w2 = Matrix::random_matrix(4,4,&mut rand);
-        let b2 = Matrix::random_matrix(1,4,&mut rand);
-        let w3 = Matrix::random_matrix(4,4,&mut rand);
-        let b3 = Matrix::random_matrix(1,4,&mut rand);
-        let w4 = Matrix::random_matrix(4,4,&mut rand);
-        let b4 = Matrix::random_matrix(1,4,&mut rand);
-        ResNet { inputs, labels, rand, w1, b1, w2, b2, w3, b3, w4, b4 }
+        let layers = vec![
+            Layer { weights: Matrix::random_matrix(4, 4, &mut rand), biases: Matrix::random_matrix(1, 4, &mut rand) },
+            Layer { weights: Matrix::random_matrix(4, 4, &mut rand), biases: Matrix::random_matrix(1, 4, &mut rand) },
+            Layer { weights: Matrix::random_matrix(4, 4, &mut rand), biases: Matrix::random_matrix(1, 4, &mut rand) },
+            Layer { weights: Matrix::random_matrix(4, 4, &mut rand), biases: Matrix::random_matrix(1, 4, &mut rand) }
+        ];
+        ResNet { inputs, labels, rand, layers }
     }
 
     pub fn activation(matrix: Matrix) -> Matrix {
@@ -42,12 +39,12 @@ impl ResNet {
     }
 
     pub fn forward(&self, input: &Matrix) -> Matrix {
-        let ResNet { w1, b1, w2, b2, w3, b3, w4, b4, .. } = self;
         let input = input.clone();
-        let out1 = Self::activation(Matrix::dot(&input, &w1) + b1.clone());
-        let out2 = Self::activation(Matrix::dot(&out1, &w2) + b2.clone() + input);
-        let out3 = Self::activation(Matrix::dot(&out2, &w3) + b3.clone() + out1);
-        Self::activation(Matrix::dot(&out3, &w4) + b4.clone() + out2)
+        let layers = self.layers.clone();
+        let out1 = Self::activation(Matrix::dot(&input, &layers[0].weights) + layers[0].biases.clone());
+        let out2 = Self::activation(Matrix::dot(&out1, &layers[1].weights) + layers[1].biases.clone() + input);
+        let out3 = Self::activation(Matrix::dot(&out2, &layers[2].weights) + layers[2].biases.clone() + out1);
+        Self::activation(Matrix::dot(&out3, &layers[3].weights) + layers[3].biases.clone() + out2)
     }
 
 }
